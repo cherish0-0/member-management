bf89feb02ad3692ebb7e74c5fe239e419a6dc667
cherish0-0 <jiyu01160@naver.com>
2025-07-01 01:29:30 +0900

    chore: 프로젝트 기본 설정

---
+++ src/main/java/goorm/member_management/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package goorm.member_management.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(PathRequest.toH2Console()).permitAll()
+				.anyRequest().permitAll()
+			)
+			.build();
+	}
+
+}

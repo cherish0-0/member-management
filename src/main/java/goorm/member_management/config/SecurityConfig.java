860b1c626f481084d998f7919e8906f3c19a3ba2
cherish0-0 <jiyu01160@naver.com>
2025-07-03 13:54:45 +0900

    feat: BCryptPasswordEncoder를 이용해 비밀번호 암호화 feat: 내부 메서드에 트랜잭션 경계 설정

--- src/main/java/goorm/member_management/config/SecurityConfig.java
+++ src/main/java/goorm/member_management/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,6 +26,11 @@ public class SecurityConfig {
 				.anyRequest().permitAll()
 			)
 			.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/goorm/member_management/member/service/AuthService.java
+++ src/main/java/goorm/member_management/member/service/AuthService.java
@@ -1,6 +1,8 @@
 package goorm.member_management.member.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import goorm.member_management.error.dto.ErrorCode;
 import goorm.member_management.error.exception.CustomException;
@@ -12,7 +14,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class AuthService {
+
 	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	// @Transactional : 프록시 우회로 트랜잭션이 적용되지 않는 내부 메서드에서 트랜잭션을 적용하기 위해 사용
+	// 데이터베이스 작업이 성공적으로 완료되면 커밋되고, 예외 발생 시 롤백됨
+	@Transactional
 	public void createMember(MemberCreateRequest request) {
 
 		// 이메일 중복 체크
@@ -20,7 +28,11 @@ public class AuthService {
 			throw new CustomException(ErrorCode.DUPLICATE_EMAIL);
 		}
 
-		Member member = new Member(request.name(), request.email(), request.password(), RoleType.USER);
+		// 비밀번호 암호화
+		String encodedPassword = passwordEncoder.encode(request.password());
+
+		Member member = new Member(request.name(), request.email(), encodedPassword, RoleType.USER);
+
 		memberRepository.save(member);
 	}
 }

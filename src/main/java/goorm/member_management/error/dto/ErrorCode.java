04ee76f598d275f361341fa7b44f5833d3924d4a
cherish0-0 <jiyu01160@naver.com>
2025-07-04 00:21:04 +0900

    refactor: DUPLICATE_EMAIL 에러 코드 변경

--- src/main/java/goorm/member_management/error/dto/ErrorCode.java
+++ src/main/java/goorm/member_management/error/dto/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
 	INTERNAL_SERVER_ERROR("서버 내부 오류입니다.", 500),
 
 	// create 관련 에러
-	DUPLICATE_EMAIL("이미 존재하는 이메일입니다.", 400);
+	DUPLICATE_EMAIL("이미 존재하는 이메일입니다.", 409);
 
 	private final String message;
 	private final int code;

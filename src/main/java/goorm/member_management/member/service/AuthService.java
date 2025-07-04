b345632fbcd5e89e51f8ef9bebb5803ecdc1fa7c
cherish0-0 <jiyu01160@naver.com>
2025-07-05 00:26:31 +0900

    refactor: 불필요한 @Transactional 어노테이션 제거

--- src/main/java/goorm/member_management/member/service/AuthService.java
+++ src/main/java/goorm/member_management/member/service/AuthService.java
@@ -2,7 +2,6 @@ package goorm.member_management.member.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import goorm.member_management.error.dto.ErrorCode;
 import goorm.member_management.error.exception.CustomException;
@@ -19,9 +18,6 @@ public class AuthService {
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
 
-	// @Transactional : 프록시 우회로 트랜잭션이 적용되지 않는 내부 메서드에서 트랜잭션을 적용하기 위해 사용
-	// 데이터베이스 작업이 성공적으로 완료되면 커밋되고, 예외 발생 시 롤백됨
-	@Transactional
 	public void createMember(MemberCreateRequest request) {
 
 		// 이메일 중복 체크

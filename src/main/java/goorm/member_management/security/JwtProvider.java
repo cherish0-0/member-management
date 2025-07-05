c79078250de12aeebf495af4e67d74039d6fe16f
cherish0-0 <jiyu01160@naver.com>
2025-07-05 19:10:09 +0900

    feat: JWT 토큰 생성 로직 추가 feat: TokenInfo 생성 chore: JWT 시크릿 및 만료 시간 임시 설정

---
+++ src/main/java/goorm/member_management/security/JwtProvider.java
@@ -0,0 +1,68 @@
+package goorm.member_management.security;
+
+import java.security.Key;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import goorm.member_management.security.dto.TokenInfo;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtProvider {
+
+	private final Key hashKey;
+	private final Duration expirationTime;
+
+	public JwtProvider(@Value("${jwt.secretKey}") String secretKey,
+		@Value("${jwt.expirationTime}") Duration expirationTime) {
+
+		// 1. Base64로 인코딩된 문자열 비밀 키를 실제 바이트 배열로 디코딩
+		// JWT 서명 알고리즘(HMAC)은 문자열이 아닌 이진 바이트 데이터를 키로 사용해야 함
+		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+
+		// 2. 디코딩된 바이트 배열을 사용하여 HMAC 서명에 적합한 SecretKey 객체 생성
+		// Keys.hmacShaKeyFor()는 바이트 배열을 기반으로 JWS(JSON Web Signature)에서
+		// 사용될 수 있는 안전한 Key 인스턴스를 생성하고 관리함
+		this.hashKey = Keys.hmacShaKeyFor(keyBytes);
+
+		this.expirationTime = expirationTime;
+	}
+
+	/**
+	 * JWT 토큰 생성
+	 * - setSubject: 토큰 페이로드(속성)의 주체 설정
+	 * - claim: 페이로드의 속성 설정 (키, 값 쌍으로 저장)
+	 * - compact: JWT 토큰 문자열로 변환
+	 */
+	public TokenInfo createAccessToken(String email) {
+		final String tokenId = UUID.randomUUID().toString();
+		final Date issuedAt = Date.from(Instant.now());
+		final Date expiration = Date.from(issuedAt.toInstant().plus(getExpirationTime()));
+
+		String token = Jwts.builder()
+			.setSubject(email)
+			.claim("tokenId", tokenId)
+			.signWith(getHashKey(), SignatureAlgorithm.HS512)
+			.setIssuedAt(issuedAt)
+			.setExpiration(expiration)
+			.compact();
+
+		return new TokenInfo(token, expiration, email, tokenId);
+	}
+
+	public Key getHashKey() {
+		return hashKey;
+	}
+
+	public Duration getExpirationTime() {
+		return expirationTime;
+	}
+}
